edc4b35815abaf2d259a5b428f8864d3ce329ff8
Bob Hart <77585854+bhart17@users.noreply.github.com>
2023-04-29 20:49:08 +0100

    Revert "qt5 syntax differences"
    This reverts commit 84e2f60ce0ccc1c53bfa774c3dce6291d0f04595.

--- src/drawAreaSend.cpp
+++ src/drawAreaSend.cpp
@@ -4,13 +4,13 @@ DrawAreaSend::DrawAreaSend(QWidget* parent) : DrawArea(parent){
 };
 
 void DrawAreaSend::mousePressEvent(QMouseEvent* event) {
-    emit startLineSig(event->pos());
-    startLine(event->pos());
+    emit startLineSig(event->position());
+    startLine(event->position());
 }
 
 void DrawAreaSend::mouseMoveEvent(QMouseEvent* event) {
-    emit continueLineSig(event->pos());
-    continueLine(event->pos());
+    emit continueLineSig(event->position());
+    continueLine(event->position());
 }
 
 // void DrawAreaSend::mouseReleaseEvent(QMouseEvent* event) {
